2ac7067bce656f2699d106d26302bd0daeb2c704
pravindnikam07 <pravindnikam07@gmail.com>
2025-06-16 11:03:15 +0530

    ITA2 16062025

---
+++ OOP_Java/IT3A/A2/_1_practical/_1606_Main.java
@@ -0,0 +1,81 @@
+
+// import java.util.*;
+import java.util.Scanner;
+
+
+public class _1606_Main {
+  
+  public static void main(String[] args) {
+    
+    // System.out.println("Ravikant");
+    // System.out.println("PIET");
+    // System.out.println("2nd");
+    
+    // System.out.println("==============");
+    
+    // System.out.println("Name: Ravikant");
+    // System.out.println("College: PIET");
+    // System.out.println("Year: 2nd");
+    
+    // System.out.println("Name: Ravikant");
+    // System.out.println("Name: Ravikant");
+    // System.out.println("Name: Ravikant");
+    // System.out.println("Name: Ravikant");
+    
+    
+    // System.out.println("============");
+    
+    // String name = "Ravi";
+    // String collegeName = "PIET";
+    // int year = 2;
+    
+    // System.out.println("Name: " + name);
+    // System.out.println("College: " + collegeName);
+    // System.out.println("Year: " + year);
+    
+    // System.out.println("Name: " + name);
+    // System.out.println("Name: " + name);
+    // System.out.println("Name: " + name);
+    // System.out.println("Name: " + name);
+    
+    
+    // System.out.println("============");
+    
+    
+    
+    // class_name obj_name = new class_name(parameter_optional);
+    
+    Scanner input = new Scanner(System.in);
+    
+    
+    // System.out.println(input.next());
+    
+    // System.out.println(input.next());
+    
+    
+    // String name = input.next();
+    // String lastName = input.next();
+    // int year = input.nextInt();
+    
+    // System.out.print(name + " ");
+    // System.out.println(lastName);
+    
+    // // System.out.println();
+    // System.out.println(year);
+    
+    
+    String name = input.nextLine();
+    int year = input.nextInt();
+    // float marks = input.nextFloat();
+    double marks = input.nextDouble();
+    System.out.println(name);
+    System.out.println(year);
+    System.out.println(marks);
+  
+  
+  
+  
+  // WAP to print college details by taking user input
+    
+  }
+}
