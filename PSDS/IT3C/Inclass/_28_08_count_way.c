be74fda7ea26bdae3ce7feda4c9b0305a4845bfb
pravindnikam07 <pravindnikam07@gmail.com>
2025-08-22 13:05:03 +0530

    done

--- PSDS/IT3C/Inclass/_28_08_count_way.c
+++ PSDS/IT3C/Inclass/_28_08_count_way.c
@@ -2,20 +2,24 @@
 
 #include <stdio.h>
 
-int countWays(int n) {
+int countWays(int n)
+{
   // base condition: if n is 0 then return 1 or if n is less than 0 return 0
 
-  if(n == 0) {
+  if (n == 0)
+  {
     return 1;
   }
-  if(n < 0) {
+  if (n < 0)
+  {
     return 0;
   }
 
   return countWays(n - 1) + countWays(n - 2);
 }
 
-int main() {
+int main()
+{
   printf("%d\n", countWays(1)); // 1 => 1 way
   printf("%d\n", countWays(2)); // 11 2 => 2 ways
   printf("%d\n", countWays(3)); // 111 12 21 => 3 ways
